a9b4faf47f1fdbc021591e962db1929cf03e3781
Eduarda Veloso <97298312+dxude@users.noreply.github.com>
2025-03-18 21:30:56 +0000

    tentando deixar codigo mais limpo

--- Atividade3/Main.java
+++ Atividade3/Main.java
@@ -20,7 +20,6 @@ public class Main {
         for (int i = 0; i < numPessoas; i++) {
             System.out.println("Pessoa " + (i + 1) + ":");
             
-            // Leitura dos dados da pessoa
             System.out.print("Sexo (M/F): ");
             char sexo = sc.next().charAt(0);
             
@@ -30,16 +29,15 @@ public class Main {
             System.out.print("Peso: ");
             double peso = sc.nextDouble();
             
-            // Verificando a pessoa mais pesada
             if (peso > pesoMaisPesado) {
                 pesoMaisPesado = peso;
                 idadeMaisPesada = idade;
             } else if (peso == pesoMaisPesado) {
-                // Se houver empate, pega a idade da última pessoa
+            
                 idadeMaisPesada = idade;
             }
             
-            // Calculando a média de peso dos homens com menos de 30 anos
+            
             if (sexo == 'M' || sexo == 'm') {
                 if (idade < 30) {
                     somaPesoHomensMenos30 += peso;
@@ -47,7 +45,7 @@ public class Main {
                 }
             }
             
-            // Calculando o percentual de mulheres com menos de 50 kg
+            
             if (sexo == 'F' || sexo == 'f') {
                 totalMulheres++;
                 if (peso < 50) {
@@ -56,19 +54,19 @@ public class Main {
             }
         }
         
-        // Calculando a média de peso dos homens com menos de 30 anos
+        
         double mediaPesoHomensMenos30 = 0;
         if (totalHomensMenos30 > 0) {
             mediaPesoHomensMenos30 = somaPesoHomensMenos30 / totalHomensMenos30;
         }
         
-        // Calculando o percentual de mulheres com menos de 50 kg
+        
         double percentualMulheresMenos50Kg = 0;
         if (totalMulheres > 0) {
             percentualMulheresMenos50Kg = (mulheresMenos50Kg * 100.0) / totalMulheres;
         }
         
-        // Exibindo os resultados
+        
         System.out.println("\nResultado:");
         System.out.println("Idade da pessoa mais pesada: " + idadeMaisPesada);
         System.out.println("Média de peso dos homens com menos de 30 anos: " + mediaPesoHomensMenos30);
