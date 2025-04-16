c67d06004f10441e6f65bc1952d8e6950883ac77
Eduarda Veloso <97298312+dxude@users.noreply.github.com>
2025-04-16 02:23:06 +0000

    ordena vetor

---
+++ simuladoDeProva/OrdenaVetor.java
@@ -0,0 +1,11 @@
+public static void ordenar(int[] v, int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - 1 - i; j++) {
+            if (v[j] > v[j + 1]) {
+                int temp = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = temp;
+            }
+        }
+    }
+}
